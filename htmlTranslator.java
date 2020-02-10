81e41a2fb4e99eac7ab2807f9661f7ba54bdd076
mcallist <mcallist@cs.dal.ca>
2020-02-10 09:30:44 -0400

    Write enough code to run through the whole file and print it to the screen

--- htmlTranslator.java
+++ htmlTranslator.java
@@ -1,6 +1,37 @@
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
 
 public class htmlTranslator {
+	// Translate the contents of a file from the CMS notation
+	// to html 1.0 format.
+
+	private static void translateFile( String filename ) {
+		BufferedReader userfile = null;
+		String inputLine = "";
+
+		// Iterate through the file, one line at a time.
+
+		try {
+			userfile = new BufferedReader( new FileReader( filename ) );
+
+			while ( (inputLine = userfile.readLine()) != null) {
+				System.out.println( inputLine );
+			}
+		} catch (Exception e) {
+			System.out.println( e.getMessage() );
+		} finally {
+			// Close the file, if it was opened.
+
+			try {
+				userfile.close();
+			} catch (Exception error) {
+				System.out.println( error.getMessage() );
+			}
+		}
+	}
+
 	public static void main( String[] args ) {
 		String filename;
 		Scanner userInput = new Scanner( System.in );
@@ -16,7 +47,7 @@ public class htmlTranslator {
 		// something.
 
 		if (filename.length() > 0) {
-			System.out.println( "filename retrieved: "+filename );
+			translateFile( filename );
 		} else {
 			System.out.println( "No filename provided." );
 		}
