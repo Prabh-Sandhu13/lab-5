46a3927d7e77bd5649afa0f4ef5f0e27d11efc27
mcallist <mcallist@cs.dal.ca>
2020-02-10 09:30:44 -0400

    Split main() from the rest of the code.  Include code to split the body into paragraphs / lists

--- htmlTranslator.java
+++ htmlTranslator.java
@@ -2,9 +2,22 @@ import java.util.Scanner;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.BufferedReader;
+import java.util.Stack;
+import java.util.Map;
 
 public class htmlTranslator {
 	private static final String titleMarker = "title: ";
+	private static final String listStartMarker = "-";
+	private static final int PARAGRAPH = 1;
+	private static final int UNORDEREDLIST = 2;
+
+	private static final int NoPart = 1;
+	private static final int InParagraph = 2;
+	private static final int InUnorderedList = 3;
+
+	private int blockStatus = NoPart;
+	private Stack<Integer> tagList = new Stack<Integer>();
+	private Map<Integer, String> tagConvert = Map.of( PARAGRAPH, "p", UNORDEREDLIST, "ul");
 
 	// Determine if a given input line matches the format for a title
 	// for the document;
@@ -35,10 +48,61 @@ public class htmlTranslator {
 		System.out.println("</title>\n</head>");
 	}
 
+	// Close all tags of a paragraph or an unordered list as if that block is ending.
+
+	private void closeTextTags() {
+		while (!tagList.empty()) {
+			System.out.println( "</"+tagConvert.get(tagList.pop())+">");
+		}
+	}
+
+	// Determine whether or not a line of text is an item for a list.
+
+	private Boolean isListItem( String line ) {
+		Boolean isList = false;
+
+		// All list items are defined to start with a particular list marker 
+		// at the start of the line.
+
+		if (listStartMarker.equals(line.substring(0, 1))) {
+			isList = true;
+		}
+
+		return isList;
+	}
+
+	// Translate a single line of a file from the CMS notation to
+	// html 1.0 format.  The method uses accumulated context from
+	// previous lines, which piles up in the class' attributes.
+
+	private void translateBodyLine( String line ) {
+		if (line.length() > 0) {
+			// Check to see if we're starting any block of text in the body
+			if (blockStatus == NoPart) {
+				int toStart = PARAGRAPH; // default block of text
+
+				blockStatus = InParagraph;
+				// The only other kind of block is an unordered list.  Check for that.
+				if (isListItem(line)) {
+					toStart = UNORDEREDLIST;
+					blockStatus = InUnorderedList;
+				}
+
+				// Start the block with appropriate tags and record of nesting.
+				System.out.println("<"+tagConvert.get(toStart)+">");
+				tagList.push( toStart );
+			}
+
+			// Now handle the line itself.
+
+			System.out.println( line );
+		}
+	}
+
 	// Translate the contents of a file from the CMS notation
 	// to html 1.0 format.
 
-	private static void translateFile( String filename ) {
+	public void translateFile( String filename ) {
 		BufferedReader userfile = null;
 		String inputLine = "";
 
@@ -66,17 +130,38 @@ public class htmlTranslator {
 				inputLine = userfile.readLine();
 			}
 
+			// Know that we will have some web page body, even if empty.
+			// Start that part of the document.
+
+			System.out.println("<body>");
+
 			// Iterate through the body of the file, one line at a time.
 			// Recall that we already have one line in memory.
 
 			while ( inputLine != null) {
-				System.out.println( inputLine );
+				// Blank lines signal a transition.  Close whatever we're in.
+				// Make the choice that multiple blank lines in a row will represent a 
+				// break in style, rather than multiple empty paragraphs.
+
+				if (inputLine.length() == 0) {
+					if (blockStatus != NoPart) {
+						closeTextTags();
+						blockStatus = NoPart;
+					}
+				} else {
+					translateBodyLine( inputLine );
+				}
+
 				inputLine = userfile.readLine();
 			}
 
-			// Close the HTML tag
+			// Close any outstanding paragraph or list tags.
 
-			System.out.println("</html>");
+			closeTextTags();
+
+			// Close the body and HTML tags
+
+			System.out.println("</body>\n</html>");
 		} catch (Exception e) {
 			System.out.println( e.getMessage() );
 		} finally {
@@ -87,27 +172,6 @@ public class htmlTranslator {
 			} catch (Exception error) {
 				System.out.println( error.getMessage() );
 			}
-		}
-	}
-
-	public static void main( String[] args ) {
-		String filename;
-		Scanner userInput = new Scanner( System.in );
-
-		// Get the filename from the user
-
-		System.out.println( "filename? " );
-		filename = userInput.next();
-		userInput.close();
-
-		// Ensure that the user provided some input.  Using a scanner,
-		// the system should keep asking for input until it gets 
-		// something.
-
-		if (filename.length() > 0) {
-			translateFile( filename );
-		} else {
-			System.out.println( "No filename provided." );
 		}
 	}
 }
